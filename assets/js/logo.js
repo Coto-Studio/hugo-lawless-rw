1cf3a938b3689c39540c2f942488a98b44ce7133
Nelson/Roberto <git@nelsonroberto.com>
2022-03-31 12:25:22 -0400

    Logo resizes using js

---
+++ assets/js/logo.js
@@ -0,0 +1,13 @@
+function resizeLogo(width) {
+  if (width.matches) {
+    // If media query matches
+    logo.setAttribute("viewBox", "0 0 870 1000");
+  } else {
+    logo.setAttribute("viewBox", "0 0 3221 1000");
+  }
+}
+
+const logo = document.getElementsByTagName("svg")[0];
+const windowWidth = window.matchMedia("(max-width: 51.75em)");
+resizeLogo(windowWidth); // Call listener function at run time
+windowWidth.addListener(resizeLogo);
